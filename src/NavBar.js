30053da585d5a019a31fcf4158b8048e586d59c0
Neurolabs Guest <neurolabs-guest@Neurolabss-MacBook-Air.local>
2025-02-24 15:56:08 +0000

    Adds Neurolabs branding

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src="https://autogenai.com/wp-content/uploads/2023/09/AutogenAI-logo-white-768x156.png" alt="Logo" /> {/* Replace with your logo path */}
+        <img src="https://cdn.prod.website-files.com/66d601cb0c6fc43926868bed/66f4a9dc6da01e9c081fb818_logo-hero.svg" alt="Logo" /> {/* Replace with your logo path */}
       </div>
       <div className="nav-info">
         Feedback? email agimson@neurolabs.ai
