97f6202e03c4de12df51a22def6421814f343d9b
Neurolabs Guest <neurolabs-guest@neurolabss-air.lan>
2025-03-13 10:44:11 +0000

    adds new PDFs

--- src/pdfConfig.js
+++ src/pdfConfig.js
@@ -4,24 +4,32 @@ const pdfCategories = {
       { name: "Heineken", path: "pdfs/front_covers/Heineken.pdf" },           
     ],
     "Back Cover": [
-      { name: "Back Cover", path: "pdfs/back_pages/back_cover.pdf" },
+      { name: "Back Cover", path: "pdfs/back_pages/remus.pdf" },
+      { name: "Back Cover", path: "pdfs/back_pages/charlie.pdf" },
+      { name: "Back Cover", path: "pdfs/back_pages/rares.pdf" },
+      { name: "Back Cover", path: "pdfs/back_pages/thomas.pdf" },                  
     ],
     "Executive Summary": [
         { name: "Executive Summary", path: "pdfs/executive_summary/executive_summary.pdf" },         
     ],            
     "Company Overview": [
-        { name: "Overview", path: "pdfs/company_overview/company_overview.pdf" },    
+        { name: "Overview", path: "pdfs/company_overview/company_overview.pdf" },
+        { name: "Office Locations", path: "pdfs/company_overview/office_locations.pdf" },      
     ],    
     "Founders Vision": [
         { name: "Founders vision", path: "pdfs/founders_vision/founders_vision.pdf" },
     ], 
     "ZIA Introduction": [
         { name: "Introduction to ZIA", path: "pdfs/zia_introduction/zia_introduction.pdf" },
-        { name: "Catalogue & Capture", path: "pdfs/zia_introduction/c_and_c.pdf" },       
+        { name: "The ZIA catalogue", path: "pdfs/zia_introduction/zia_catalogue.pdf.pdf" },       
+        { name: "The Power of Synthetically Generated Images", path: "pdfs/zia_introduction/the_power_of_syntheticall_generated_images.pdf" },           
     ], 
     "Neurolabs architecture": [
         { name: "Neurolabs Architecture", path: "pdfs/neurolabs_architecture/neurolabs_architecture.pdf" }
-        ],       
+        ],  
+    "Support & Maintenance": [
+        { name: "Support & Maintenance", path: "pdfs/support/support_and_maintenance.pdf" }
+        ],                  
     "Timelines": [
         { name: "Integration timeilne", path: "pdfs/timelines/timeline.pdf" },        
     ],         
@@ -40,7 +48,10 @@ const pdfCategories = {
         { name: "Level 1", path: "pdfs/use_cases/level_1.pdf" },
         { name: "Level 2 & 3", path: "pdfs/use_cases/level_2_and_3.pdf" },
         { name: "Example Display & Promotional", path: "pdfs/use_cases/use_case_example_display_and_promotional.pdf" },       
-    ],                                           
+    ],    
+    "Commercials": [
+        { name: "Commercial framework", path: "pdfs/commercials/commercial_framework.pdf" },       
+    ],    
   };
   
   export default pdfCategories;
